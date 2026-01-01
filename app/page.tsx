d8205b3d2eac878c381eb61659bef1a083911135
aziziabdallahesmail-ensia <abdellah.esmail.azizi@ensia.edu.dz>
2026-01-01 13:16:22 +0100

    updated the landing page UI (still needs further change)

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,6 @@
-import { AuthButton } from "@/components/auth-button";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import LandingPage from "@/components/landing-page";
+import { redirect } from "next/navigation";
 
-
+// Redirect root to the (index) route group's home page
 export default function Home() {
-  return (
-    <main className="min-h-screen flex flex-col items-center">
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm ">
-          <AuthButton />
-          </div>
-        </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          <LandingPage />
-        </div>
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-4">
-         
-          <ThemeSwitcher />
-        </footer>
-      </div>
-    </main>
-  );
+  redirect("/home");
 }
